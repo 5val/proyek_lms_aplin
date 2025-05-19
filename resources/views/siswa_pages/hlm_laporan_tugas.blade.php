acea5344eb99f81e288af84e14fed13da1085b61
YukiUniverse <ovaldooentoro29@gmail.com>
2025-05-19 14:45:22 +0700

    perbaikan siswa

--- resources/views/siswa_pages/hlm_laporan_tugas.blade.php
+++ resources/views/siswa_pages/hlm_laporan_tugas.blade.php
@@ -4,6 +4,18 @@
 <div class="p-3">
     <h4 class="mb-3">Laporan Nilai Tugas</h4>
 
+    <!-- Dropdown Pilih Periode -->
+    <form method="GET" action="{{ route('siswa.laporan_tugas') }}" class="mb-4">
+        <label for="periodeSelect" class="form-label">Pilih Periode:</label>
+        <select name="periode" id="periodeSelect" class="form-select" onchange="this.form.submit()">
+            @foreach($daftarPeriode as $periode)
+                <option value="{{ $periode->ID_PERIODE }}" {{ $selectedPeriode == $periode->ID_PERIODE ? 'selected' : '' }}>
+                    {{ $periode->PERIODE }}
+                </option>
+            @endforeach
+        </select>
+    </form>
+
     <!-- Tasks that have been submitted (Table) -->
     <h5>Sudah Dikumpulkan</h5>
     <table class="table table-bordered mb-4">
@@ -29,6 +41,7 @@
     <h5>Belum Dikumpulkan</h5>
     <div class="row">
         @foreach ($tugasBelumDikirim as $t)
+        <a href="{{ url('/siswa/hlm_detail_tugas/' . urlencode($t->ID_TUGAS)) }}" class="text-decoration-none text-dark">
             <div class="col-md-4 mb-3">
                 <div class="task-card">
                     <h5>{{ $t->NAMA_PELAJARAN }} - {{ $t->NAMA_TUGAS }}</h5>
@@ -36,14 +49,13 @@
                     <span class="task-status status-belum">Belum Dikumpulkan</span>
                 </div>
             </div>
+        </a>
         @endforeach
     </div>
 
-    <!-- Card rata-rata -->
+    <!-- Rata-rata Nilai -->
     <div class="average-card-custom">
         <h5 class="average-card-title">Rata-Rata Nilai per Mata Pelajaran</h5>
-        
-        <!-- Table for average scores -->
         <table class="average-table table-bordered table-lg">
             <thead class="table-header-custom">
                 <tr>
